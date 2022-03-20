3759211cae67cefc5c237b2b3d19629da0ad9667
john maxwell <john56@gmail.com>
2022-03-20 12:16:09 +0530

    example file, make file, multifile

---
+++ Makefile
@@ -0,0 +1,7 @@
+#Tathetname : Dependent target names
+#<TAB>command
+Build:
+	gcc firstcode1.c test_add_integers1.c unity/unity.c -Iunity
+
+Run:
+	./a.out
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a=0,b=0;
+    printf("enter two numbers\n");
+    scanf("%d %d",&a,&b);
+    int sum=a+b;
+    printf("result: %d",sum);
+
+}
